bd99becab56bc96b45c8ee044c26816a05b86860
marqh <markh@metarelate.net>
2015-08-06 10:05:30 +0000

    bsd 3 clause

--- src/wgdos_expand_row_to_data.c
+++ src/wgdos_expand_row_to_data.c
@@ -1,5 +1,5 @@
 /*
-# Copyright (c) 2012, The Met Office, UK
+# Copyright (c) 2012 - 2015, The Met Office, UK
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -10,6 +10,9 @@
 # 2. Redistributions in binary form must reproduce the above copyright notice,
 #    this list of conditions and the following disclaimer in the documentation
 #    and/or other materials provided with the distribution. 
+# 3. Neither the name of copyright holder nor the names of any
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -21,10 +24,6 @@
 # ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# The views and conclusions contained in the software and documentation are those
-# of the authors and should not be interpreted as representing official policies, 
-# either expressed or implied, of the Met Office.
 #
 */
 /* wgdos_expand_row_to_data.c
